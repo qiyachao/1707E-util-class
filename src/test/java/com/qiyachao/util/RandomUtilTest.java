14a7a2eb092b8c7770af28871e5ce3cde56550e9
qyc <553603097@qq.com>
2019-11-07 19:24:14 +0800

    随机数工具类和字符串工具类

---
+++ src/test/java/com/qiyachao/util/RandomUtilTest.java
@@ -0,0 +1,41 @@
+package com.qiyachao.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class RandomUtilTest {
+
+	@Test
+	public void testRandom() {
+		for (int i = 0; i < 10; i++) {
+			int r = RandomUtil.random(10, 20);
+			System.out.println(r);
+		}
+	}
+
+	@Test
+	public void testSubRandom() {
+		for (int i = 0; i < 10; i++) {
+			RandomUtil.subRandom(1, 10, 3);
+		}
+		
+	}
+
+	@Test
+	public void testRandomCharacter() {
+		for (int i = 0; i < 100; i++) {
+			char c = RandomUtil.randomCharacter();
+			
+			System.out.println(c);
+			}
+	}
+
+	@Test
+	public void testRandomString() {
+		String string = RandomUtil.randomString(4);
+		System.out.println(string);
+	}
+
+}
---
+++ src/test/java/com/qiyachao/util/StringUtilTest.java
@@ -0,0 +1,38 @@
+package com.qiyachao.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class StringUtilTest {
+
+	@Test
+	public void testHasLength() {
+		boolean b = StringUtil.hasLength("");
+		System.out.println(b);
+	}
+
+	@Test
+	public void testHasText() {
+		String str="ab c ";
+		boolean b = StringUtil.hasText(str);
+		System.out.println(b);
+	}
+
+	@Test
+	public void testRandomChineseString() {
+		String name = StringUtil.randomChineseString(10);
+		System.out.print(name );
+	}
+
+	@Test
+	public void testGenerateChineseName() {
+		for (int i = 0; i < 10; i++) {
+			String name = StringUtil.generateChineseName();
+			System.out.print(name +"   " );
+			
+		}
+	}
+
+}
